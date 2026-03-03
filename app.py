8cdbc6f40b2b3cd61127f95c19ce0dd2f457658d
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-03-03 16:08:18 +0100

    format of eta selector

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ selected_E = st.sidebar.selectbox("Energy (E)", options=E_options)
 
 # create dropwdown for eta
 eta_options = np.asanyarray(data.attrs["ETA"])
-selected_eta = st.sidebar.selectbox("Eta", options=eta_options).round(6)
+selected_eta = st.sidebar.selectbox("Select eta", options=range(len(eta_options)),
+        format_func = lambda i: f"{eta_options[i]:.1e}")
 
 # create slider for site to investigate
 selected_site = st.sidebar.slider("Site Index", min_value=0, max_value=selected_N-1, value=0)
